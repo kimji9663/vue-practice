70b15845895e3134dae5b9e56ce79b56f7e8fe1c
Jihye Kim <rlawlgp9663@naver.com>
2024-01-24 15:55:19 +0900

    vue 기초 문법 연습

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hellow {{ animal }}</h1>
+    <h2>{{ animal }} like {{ food }}.</h2>
+    <input type="text" v-model="food" />
+    <img v-bind:src="imageSource" alt="vue" />
+    <a :href="googleUrl">Google</a>
+    <hr />
+    <h2 v-bind:class="{red: food==='banana', 'not-good': food==='rice'}">{{ animal }} like {{ food }}!</h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      animal: "Monkey",
+      food: "banana",
+      imageSource: "https://www.vuemastery.com/_nuxt/image/d26370.svg",
+      googleUrl: "https://www.google.com/",
+    }
   }
 }
 </script>
@@ -22,5 +33,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.red {
+  color: #ff0000;
+}
+.not-good {
+  text-decoration: line-through;
 }
 </style>
