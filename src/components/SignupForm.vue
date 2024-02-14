cea1dae6ecab4201881341a05d26d5d89ec73830
Jihye Kim <rlawlgp9663@naver.com>
2024-02-14 15:21:52 +0900

    입력 필드 추가, 파일 삭제

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <form action=""></form>
+    <form>
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input id="password" type="text">
+      </div>
+      <div>
+        <label for="nickname">nick: </label>
+        <input id="nickname" type="text">
+      </div>
+      <button type="submit">로그인</button>
+    </form>
   </div>
 </template>
 
